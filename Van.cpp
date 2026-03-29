cb7fbc4e0d17ee013d9c5694f36bc9a1e001e6ab
PryimakYaroslav <anonimusua6@gmail.com>
2026-03-29 14:09:05 +0300

    Implemented Constructor and Destructor for Van

--- Van.cpp
+++ Van.cpp
@@ -1,1 +1,24 @@
 #include "Van.h"
+#include <iostream>
+
+Van::Van() : Vehicle(), 
+    cargoCapacity(0.0), 
+    hasUSBports(false), 
+    hasConditioner(false) {}
+
+Van::Van(int id, std::string model, int countOfSeats, double cargoCapacity, bool hasUSBports, bool hasConditioner) : 
+    Vehicle (id, model, countOfSeats),
+    cargoCapacity(cargoCapacity),
+    hasUSBports(hasUSBports),
+    hasConditioner(hasConditioner) {}
+
+Van::Van(int id, std::string model, int countOfSeats, std::string routStart, std::string destination, long run,
+    double cargoCapacity, bool hasUSBports, bool hasConditioner) :
+    Vehicle(id, model, countOfSeats, routStart, destination, run),
+    cargoCapacity(cargoCapacity),
+    hasUSBports(hasUSBports),
+    hasConditioner(hasConditioner) {}
+
+Van::~Van(){
+    std::cout << "Van destroyed" << std::endl;
+}
--- Van.h
+++ Van.h
@@ -7,4 +7,11 @@ class Van : public Vehicle{
     double cargoCapacity;
     bool hasUSBports;
     bool hasConditioner;
+
+    public:
+
+    Van();
+    Van(int id, std::string model, int countOfSeats, double cargoCapacity, bool hasUSBports, bool hasConditioner);
+    Van(int id, std::string model, int countOfSeats, std::string routStart, std::string destination, long run,
+    double cargoCapacity, bool hasUSBports, bool hasConditioner);
 };

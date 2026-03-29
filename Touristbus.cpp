33711df15179570781f4a7091f4dbd10b4b36043
PryimakYaroslav <anonimusua6@gmail.com>
2026-03-29 18:15:05 +0300

    Implemated Constructor and Destructor for Touristbus

--- Touristbus.cpp
+++ Touristbus.cpp
@@ -1,1 +1,24 @@
 #include "Touristbus.h"
+#include <iostream>
+
+Touristbus::Touristbus() : Vehicle(),
+    numberOfScreens(0),
+    hasToilet(false),
+    isDoubleDeckered(false) {};
+
+Touristbus::Touristbus(int id, std::string model, int countOfSeats, int numberOfScreens, bool hasToilet, bool isDoubleDeckered) :
+    Vehicle(id, model, countOfSeats),
+    numberOfScreens(numberOfScreens),
+    hasToilet(hasToilet),
+    isDoubleDeckered(isDoubleDeckered) {};
+
+Touristbus::Touristbus(int id, std::string model, int countOfSeats, std::string routStart, std::string destination, long run, 
+    int numberOfScreens, bool hasToilet, bool isDoubleDeckered) :
+    Vehicle(id, model, countOfSeats, routStart, destination, run),
+    numberOfScreens(numberOfScreens),
+    hasToilet(hasToilet),
+    isDoubleDeckered(isDoubleDeckered) {};
+
+Touristbus::~Touristbus(){
+    std::cout << "Touristbus destroyed" << std::endl;
+}
--- Touristbus.h
+++ Touristbus.h
@@ -7,4 +7,11 @@ class Touristbus : public Vehicle{
     int numberOfScreens;
     bool hasToilet;
     bool isDoubleDeckered;
+
+    public:
+    Touristbus();
+    Touristbus(int id, std::string model, int countOfSeats, int numberOfScreens, bool hasToilets, bool isDoubleDeckered);
+    Touristbus(int id, std::string model, int countOfSeats, std::string routStart, std::string destination, long run, 
+    int numberOfScreens, bool hasToilets, bool isDoubleDeckered);
+    ~Touristbus();
 };
